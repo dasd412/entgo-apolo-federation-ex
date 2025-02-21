19b3515da642aaebeeaee2e3ea45ba238f8a6ea3
yang-yeongjun <dasd412@naver.com>
2025-02-21 17:31:43 +0900

    server.go 리팩토링 임시 저장 (ent client 추상화가 어려워서 나중으로 미룸)

--- libs/client/ent_client.go
+++ libs/client/ent_client.go
@@ -1,35 +1,37 @@
 package client
 
 import (
-	"db"
-	"entgo.io/ent/dialect/sql"
-	"log"
-	"os"
-	"time"
-	"user/pkg/ent"
+	"context"
+	"entgo.io/ent"
 )
 
-func NewEntClient(master *sql.Driver, replica *sql.Driver) *ent.Client {
-	if os.Getenv("SERVICE_ENV") == "local" {
-		return ent.NewClient(
-			ent.Driver(
-				&db.Drivers{
-					Master:  master,
-					Replica: replica,
-				},
-			),
-			ent.Debug(),
-			ent.Log(
-				func(argument ...any) {
-					start := time.Now()
-					duration := time.Since(start)
-					log.Printf("took: %v , entgo: %v ", duration, argument)
-				},
-			),
-		)
-	} else {
-		return ent.NewClient(
-			ent.Driver(&db.Drivers{Master: master, Replica: replica}),
-		)
-	}
+type EntClient interface {
+	Close() error
+	Mutate(ctx context.Context, m ent.Mutation) (ent.Value, error)
 }
+
+// todo *ent.Client에서 "user/pkg/ent" 의존성 제거 필요
+//func NewEntClient(master *sql.Driver, replica *sql.Driver) *ent.Client {
+//	if os.Getenv("SERVICE_ENV") == "local" {
+//		return ent.NewClient(
+//			ent.Driver(
+//				&db.Drivers{
+//					Master:  master,
+//					Replica: replica,
+//				},
+//			),
+//			ent.Debug(),
+//			ent.Log(
+//				func(argument ...any) {
+//					start := time.Now()
+//					duration := time.Since(start)
+//					log.Printf("took: %v , entgo: %v ", duration, argument)
+//				},
+//			),
+//		)
+//	} else {
+//		return ent.NewClient(
+//			ent.Driver(&db.Drivers{Master: master, Replica: replica}),
+//		)
+//	}
+//}
